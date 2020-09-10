7850c24d4f0008f058a5910012cdd7abb4222441
katejkpark <64166850+katejkpark@users.noreply.github.com>
2020-09-09 23:17:39 -0400

    Delete _app.js

--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import '../styles/global.scss'
-
-// Stuff here will apply to every page, so be careful!
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
