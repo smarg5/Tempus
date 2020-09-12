6eda053069ad607d52c24ac49dfdd30c88e1656c
annatmmm <69659572+annatmmm@users.noreply.github.com>
2020-09-12 12:06:03 -0400

    LogIn/SignUp

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,6 +8,8 @@ function Navbar({ router }) {
     { text: 'Mood Tracker', href: '/moodtracker' },
     { text: 'Community', href: '/community' },
     { text: 'Therapists', href: '/therapists' },
+    { text: 'LogIn', href: '/login'},
+    { text: 'SignUp', href: '/signup'},
 
     // { text: '', href: '' },
   ];
