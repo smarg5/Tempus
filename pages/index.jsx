4c0a41b831a6c5205956a36cc7d46704ad46c74f
katejkpark <64166850+katejkpark@users.noreply.github.com>
2021-01-19 00:15:24 -0500

    Add files via upload

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
-import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 import Head from 'next/head'
+import fetch from 'isomorphic-unfetch';
+import moodStyles from '../styles/moodtracker.module.scss'
+import useSWR from 'swr';
 import cookie from 'js-cookie';
 import { useSession } from 'next-auth/client';
 
@@ -10,9 +13,15 @@ export default function Home(props) {
   const [ session, loading ] = useSession()
   console.log(session, "aslkdfjas")
 
+  let currentsession = session
+
   if (typeof window !== 'undefined' && loading) return null
-  let name = session ? session.user.name : ""
   let loggedIn = false;
+  // if (!session) {
+  //   // loggedIn = true;
+  // }
+  // console.log(session.user.name)
+  let name = session ? session.user.name : ""
 
 
 
@@ -23,7 +32,7 @@ export default function Home(props) {
       </Head>
 
       <Navbar />
-     {loggedIn && (
+     {/* {loggedIn && (
       <>
        <p>Welcome {name}!</p>
         
@@ -35,28 +44,41 @@ export default function Home(props) {
           Logout
         </button>
       </>
-    )}
+    )} */}
 
 {!loggedIn && (
       <section className="hero">
         <div className="container">
           <div className="text-wrapper">
+          <div className={moodStyles.splitScreen}>
+          <div className="dayswrapper w-full">
 
-            <h1 className="title">Hi there, 
+
+          <br></br>
+<br></br>
+            <h1 className="title">Hi there, {name.split(" ")[0]}
             <br></br>Welcome to Tempus <span class="wave">👋</span>
           
             </h1>
-            <p className="description">Join our supportive community. You are not alone.</p>
-            <p className="description">Interact with our mood tracker, blog platform, therapist locator, and sign up to store your information!</p>
+            <p className="description">Join our supportive community. You are not alone!</p>
+            <p className="description">Interact with our mood tracker, community platform, therapist locator. Sign up to store your information.</p>
 
             <Link href="/about"><a className="cta">Learn More</a></Link>
           </div> </div>
-
+          </div>     <div className={moodStyles.flexRow}>
+          <img src="/images/STOCK.png" alt="me" width="480" height="450" />
+</div>
+     
+</div>
          
         {/* <>
           <Link href="/login">Login</Link>
-          <p>or</p> <Link href="/signup">Sign Up</Link> </> */} </section>
-                    )}
+          <p>or</p>
+          <Link href="/signup">Sign Up</Link>
+        </> */}
+      
+      </section>
+      )}
     </>
   );
 }
