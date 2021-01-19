61b5eebea7dcb0d497df451205aa920a365bf88d
katejkpark <64166850+katejkpark@users.noreply.github.com>
2021-01-19 18:29:07 -0500

    Add files via upload

--- pages/community.jsx
+++ pages/community.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import FlipMove from "react-flip-move"
-import Navbar from './components/Navbar';
+import Navbar from '../components/Navbar';
 import Head from 'next/head'
 import communityStyles from '../styles/community.module.scss'
 import therapistStyles from '../styles/therapists.module.scss'
@@ -47,7 +47,7 @@ class Community extends Component {
 
   async getPosts() {
     var body = {}
-    const attempt = await axios.post('https://tempusio.netlify.app/api/community/getposts', body, {
+    const attempt = await axios.post('http://localhost:3000/api/community/getposts', body, {
     });
     this.setState({
       posts: attempt.data.posts.slice(0, 3),
@@ -58,7 +58,7 @@ class Community extends Component {
     var body = {
       content: this.state.content,
     }
-    await axios.post('https://tempusio.netlify.app/api/community/submitpost', body, {
+    await axios.post('http://localhost:3000/api/community/submitpost', body, {
   });
   }
 
